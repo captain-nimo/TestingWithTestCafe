b4750f1893a261826dad5e8e36d7b6946f82de66
captain-nimo <mahmud.tester@gmail.com>
2018-07-06 23:56:07 -0400

    add utilities file
    Adding utilities file for Real Canadian Super Store website

---
+++ super_store_utilities.js
@@ -0,0 +1,38 @@
+import {Selector} from 'testcafe';
+import config from './super_store_config.json';
+
+export async function selectLocation(t) {
+    
+    await t
+        // Select location
+        .click(Selector(config.SelectProvince).withText(config.provinceName));
+}
+
+export async function closingIframe(t) {
+    
+    await t
+        // Switching to iframe and closing it
+        .switchToIframe(Selector(config.iframe))
+        .click(config.iframId)
+        .switchToMainWindow();
+}
+
+
+export async function search(t) {
+    
+    await t
+        // Inserting search text in the search bar
+        .typeText(config.searchBarId, config.searchString)
+        // Start searching
+        .pressKey('enter')
+        // Checking the search result
+        .expect(Selector(config.searchResult).innerText).contains(config.searchResultString);
+}
+
+export async function checkDealBadge(t) {
+    
+    // Checking deal badge
+    await t
+        .expect(Selector(config.badge).exists).ok();
+    
+}
