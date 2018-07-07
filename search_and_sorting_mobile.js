49d8f145939076e23b1d6f8690f352ef982949e4
Mahmudul Hasan Robin <mahmud.tester@gmail.com>
2018-07-06 23:18:21 -0400

    refactoring code

--- search_and_sorting_mobile.js
+++ search_and_sorting_mobile.js
@@ -1,59 +1,22 @@
 import {Selector} from 'testcafe';
+import config from './lobLaw_config.json';
+import {closingIframe, search, sort} from './loblaw_utilities.js';
 
-fixture('Testing LobLaw Website in mobile screen')
-    .page('https://www.loblaws.ca/');
+fixture('Loblaw web client in mobile screen')
+    .page(config.baseUrl);
 
-test('Search apples and sort the search results in smaller screen', async t => {
-        
+test('Search for items and sort the search results', async t => {
+    
     await t
-        // Re-sizing browser window
+        // Setting browser window size to iPhone 5S
         .resizeWindowToFitDevice('iPhone 5S', {
             portraitOrientation: true
         });
     
-    await t
-        // Switching to iframe and closing it
-        .wait(5000)
-        .switchToIframe(Selector('.sl-show-animation .sl-frame'))
-        .click('#i-1')
-        .switchToMainWindow();
+    await closingIframe(t); // Called function to close the iFrame
     
-    await t
-        // Inserting 'apples' text in the search bar
-        .typeText('#search-bar', 'apples')
-        // Start searching
-        .pressKey('enter')
-        
-        // Checking the search result
-        .expect(Selector('.term-result-found').innerText).contains('"APPLES"');
+    await search(t); // Called function to search item
+ 
+    await sort(t); // Called function to sort the search results
     
-        // Sorting search results from highest price to lowest price.
-        const sorting = Selector('button')
-        .withText('Price (High to Low)')
-    
-        const selected = Selector('.btn-sort-link  selected');
-    
-    await t
-        // Clicking the 'price-desc' button
-        .click(sorting);
-    
-    await t
-        .expect(selected);
-    
-    await t
-        const itemPrice = Selector('.reg-price-text')
-        const itemCount = await itemPrice.count
-        const price = new Array(itemCount);
-        
-        // Adding price values in the array
-        for (var i = 0; i < itemCount; i++) {
-            await t
-            price.push((Selector('.reg-price-text').nth(i)));
-        }
-    
-        // Checking whether the prices are sorted
-        for (var i = 0; i < itemCount; i++) {
-            await t
-                .expect(price[i] >= price[i+1]);
-        }
 });
