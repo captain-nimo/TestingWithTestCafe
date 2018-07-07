e20908497cf62889776c2edc93e35cda07aa2a0d
Mahmudul Hasan Robin <mahmud.tester@gmail.com>
2018-07-06 23:18:07 -0400

    refactoring code

--- search_and_sorting_fr.js
+++ search_and_sorting_fr.js
@@ -1,60 +1,22 @@
 import {Selector} from 'testcafe';
+import config from './lobLaw_config.json';
+import {closingIframe, search, sort} from './loblaw_utilities.js';
 
-fixture('Testing LobLaw Website in french')
-    .page('https://www.loblaws.ca/');
+fixture('Loblaw web client in french')
+    .page(config.baseUrl);
 
-test('Search apples and sort the search results in French', async t => {
+test('Search for item and sort the search results', async t => {
     
     await t
-        // Maximize browser window
-        .maximizeWindow();
+        .maximizeWindow(); // Maximize browser window
+    
+    await closingIframe(t); // Called function to close the iFrame
     
     await t
-        // Switching to iframe and closing it
-        .wait(5000)
-        .switchToIframe(Selector('.sl-show-animation .sl-frame'))
-        .click('#i-1')
-        .switchToMainWindow();
+        .click(Selector(config.languageFrench)); // Setting language as French
     
-    await t
-        .click(Selector('.header-bar [href="\/PickUpStore\/language"]'));
-        
-    await t
-        // Inserting 'apples' text in the search bar
-        .typeText('#search-bar', 'pommes')
-        // Start searching
-        .pressKey('enter')
-        
-        // Checking the search result
-        .expect(Selector('.term-result-found').innerText).contains('"POMMES"');
+    await search(t); // Called function to search item
     
-        // Sorting search results from highest price to lowest price.
-        const sorting = Selector('button')
-        .withText('Prix : élevé à faible')
+    await sort(t); // Called function to sort the search results
     
-        const selected = Selector('.btn-sort-link  selected');
-    
-    await t
-        // Clicking the 'price-desc' button
-        .click(sorting);
-    
-    await t
-        .expect(selected);
-    
-    await t
-        const itemPrice = Selector('.reg-price-text')
-        const itemCount = await itemPrice.count
-        const price = new Array(itemCount);
-        
-        // Adding price values in the array
-        for (var i = 0; i < itemCount; i++) {
-            await t
-            price.push((Selector('.reg-price-text').nth(i)));
-        }
-        
-        // Checking whether the prices are sorted
-        for (var i = 0; i < itemCount; i++) {
-            await t
-                .expect(price[i] >= price[i+1]);
-        }
 });
