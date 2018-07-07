9dd5ebe8796f344504c8976b30fbeda65cb89518
Mahmudul Hasan Robin <mahmud.tester@gmail.com>
2018-07-06 23:58:27 -0400

    Refactoring code

--- deals_en.js
+++ deals_en.js
@@ -1,38 +1,21 @@
 import {Selector} from 'testcafe';
+import config from './super_store_config.json';
+import {selectLocation, closingIframe, search, checkDealBadge} from './super_store_utilities.js';
 
-fixture('Testing Real Canadian Superstore Website')
-    .page('https://www.realcanadiansuperstore.ca/');
+fixture('Real Canadian Superstore website')
+    .page(config.baseUrl);
 
-test('Searching oranges and checking deals', async t => {
+test('Search for item and check deal badge', async t => {
     
     await t
-        // Maximize browser window
-        .maximizeWindow();
+        .maximizeWindow(); // Maximize browser window
     
-    await t
-        // Selecting location
-        .wait(5000)
-        .click(Selector('.select-province').withText('Ontario'));
+    await selectLocation(t); // Called function to select location
     
-    await t
-        // Switching to iframe and closing it
-        .wait(5000)
-        .switchToIframe(Selector('.sl-show-animation .sl-frame'))
-        .click('#i-1')
-        .switchToMainWindow();
+    await closingIframe(t); // Called function to close the iFrame
     
-    await t
-        // Inserting 'oranges' text in the search bar
-        .typeText('#search-bar', 'oranges')
-        // Start searching
-        .pressKey('enter')
+    await search(t); // Called function to search item
     
-        // Checking the search result
-        .expect(Selector('.term-result-found').innerText).contains('"ORANGES"');
+    await checkDealBadge(t); // Called function to check deal badge
     
-        // Checking deal badge
-        const badge = Selector('.deal-type');
-    
-    await t
-        .expect(badge.exists).ok();
 });
