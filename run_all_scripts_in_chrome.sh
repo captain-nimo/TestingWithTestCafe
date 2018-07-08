21434aac6325f63fee849c74e91b826ccaa9c018
captain-nimo <mahmud.tester@gmail.com>
2018-07-08 01:38:59 -0400

    Adding bash file to run all scripts in chrome

---
+++ run_all_scripts_in_chrome.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+testcafe chrome search_and_sorting_en.js
+testcafe chrome search_and_sorting_fr.js
+testcafe chrome search_and_sorting_mobile.js
+testcafe chrome deals_en.js
+testcafe chrome deals_mobile.js
+
+echo "All script execution in chrome browser successfully complete"
