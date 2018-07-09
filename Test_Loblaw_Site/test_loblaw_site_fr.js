bfd3b9b8dc367f37bdfb5ed7011520d654849cd7
Mahmudul Hasan Robin <mahmud.tester@gmail.com>
2018-07-09 02:47:15 -0400

    Refactoring

--- Test_Loblaw_Site/test_loblaw_site_fr.js
+++ Test_Loblaw_Site/test_loblaw_site_fr.js
@@ -1,6 +1,6 @@
 import {Selector} from 'testcafe';
-import config from './lobLaw_config.json';
-import {closingIframe, search, sort} from './loblaw_utilities.js';
+import config from './config.json';
+import {closingIframe, search, sort} from './utilities.js';
 
 fixture('Loblaw website in french')
     .page(config.baseUrl);
@@ -8,15 +8,15 @@ fixture('Loblaw website in french')
 test('Search for item and sort the search results', async t => {
     
     await t
-        .maximizeWindow(); // Maximize browser window
+        .maximizeWindow(); // Calling library fuction to maximize browser window
     
-    await closingIframe(t); // Called function to close the iFrame
+    await closingIframe(t); // Calling function from 'utilities.js' to close the iFrame
     
     await t
         .click(Selector(config.languageFrench)); // Setting language as French
     
-    await search(t); // Called function to search item
+    await search(t); // CCalling function from 'utilities.js' to search item
     
-    await sort(t); // Called function to sort the search results
+    await sort(t); // Calling function from 'utilities.js' to sort the search results
     
 });
