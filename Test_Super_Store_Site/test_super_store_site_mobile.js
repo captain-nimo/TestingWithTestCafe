8272521bc7862d8e6b5b3f88e17f559b6c9a0f16
Mahmudul Hasan Robin <mahmud.tester@gmail.com>
2018-07-09 16:52:55 -0400

    Refactoring

--- Test_Super_Store_Site/test_super_store_site_mobile.js
+++ Test_Super_Store_Site/test_super_store_site_mobile.js
@@ -1,6 +1,6 @@
 import {Selector} from 'testcafe';
 import config from './config.json';
-import {selectLocation, closingIframe, search, checkDealBadge} from './utilities.js';
+import {selectLocation, closingIframe, searchItem, checkDealBadge} from './utilities.js';
 
 fixture('Real Canadian Superstore website in mobile screen')
     .page(config.baseUrl);
@@ -17,8 +17,8 @@ test('Search for item and check deal badge', async t => {
     
     await closingIframe(t); // Calling function from 'utilities.js' to close the iFrame
     
-    await search(t); // Calling function from 'utilities.js' to search item
+    await searchItem(t, 'oranges', 'ORANGES'); // Calling function from 'utilities.js' to search item in the search bar
     
-    await checkDealBadge(t); // Calling function from 'utilities.js' to check deal badge
+    await checkDealBadge(t); // Calling function from 'utilities.js' to check deal badge presence in the search result page
     
 });
