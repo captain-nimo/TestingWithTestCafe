fadb4ca06ca89d78ea0c5c4386c11dc660d6f15f
Mahmudul Hasan Robin <mahmud.tester@gmail.com>
2018-07-09 16:52:46 -0400

    Refactoring

--- Test_Super_Store_Site/test_super_store_site_en.js
+++ Test_Super_Store_Site/test_super_store_site_en.js
@@ -1,6 +1,6 @@
 import {Selector} from 'testcafe';
 import config from './config.json';
-import {selectLocation, closingIframe, search, checkDealBadge} from './utilities.js';
+import {selectLocation, closingIframe, searchItem, checkDealBadge} from './utilities.js';
 
 fixture('Real Canadian Superstore website')
     .page(config.baseUrl);
@@ -14,8 +14,8 @@ test('Search for item and check deal badge', async t => {
     
     await closingIframe(t); // CCalling function from 'utilities.js' to close the iFrame
     
-    await search(t); // Calling function from 'utilities.js' to search item
+    await searchItem(t, 'oranges', 'ORANGES'); // Calling function from 'utilities.js' to search item in the search bar
     
-    await checkDealBadge(t); // Calling function from 'utilities.js' to check deal badge in the search result page
+    await checkDealBadge(t); // Calling function from 'utilities.js' to check deal badge presence in the search result page
     
 });
