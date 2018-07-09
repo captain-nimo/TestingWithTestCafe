d1c6bd591e462d3bc4d1882323953370e92f5aa9
Mahmudul Hasan Robin <mahmud.tester@gmail.com>
2018-07-09 02:48:51 -0400

    Refactoring

--- Test_Super_Store_Site/test_super_store_site_mobile.js
+++ Test_Super_Store_Site/test_super_store_site_mobile.js
@@ -1,6 +1,6 @@
 import {Selector} from 'testcafe';
-import config from './super_store_config.json';
-import {selectLocation, closingIframe, search, checkDealBadge} from './super_store_utilities.js';
+import config from './config.json';
+import {selectLocation, closingIframe, search, checkDealBadge} from './utilities.js';
 
 fixture('Real Canadian Superstore website in mobile screen')
     .page(config.baseUrl);
@@ -8,17 +8,17 @@ fixture('Real Canadian Superstore website in mobile screen')
 test('Search for item and check deal badge', async t => {
     
     await t
-        // Setting browser window size to iPhone 5S
+        // Calling library function to set browser window size to iPhone 5S
         .resizeWindowToFitDevice('iPhone 5S', {
             portraitOrientation: true
         });
     
-    await selectLocation(t); // Called function to select location
+    await selectLocation(t); // Calling function from 'utilities.js' to select location
     
-    await closingIframe(t); // Called function to close the iFrame
+    await closingIframe(t); // Calling function from 'utilities.js' to close the iFrame
     
-    await search(t); // Called function to search item
+    await search(t); // Calling function from 'utilities.js' to search item
     
-    await checkDealBadge(t); // Called function to check deal badge
+    await checkDealBadge(t); // Calling function from 'utilities.js' to check deal badge
     
 });
