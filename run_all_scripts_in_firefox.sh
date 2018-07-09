05b073ac1bf43f5eab7f25a7d3d912397dc37489
captain-nimo <mahmud.tester@gmail.com>
2018-07-09 02:31:18 -0400

    Deleting the file

--- run_all_scripts_in_firefox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-testcafe firefox search_and_sorting_en.js
-testcafe firefox search_and_sorting_fr.js
-testcafe firefox search_and_sorting_mobile.js
-testcafe firefox deals_en.js
-testcafe firefox deals_mobile.js
-
-echo "All script execution in chrome browser successfully complete"
